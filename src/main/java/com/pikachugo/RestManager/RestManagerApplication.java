5a81c8cce2913bad772c959b23215e71a546a9ca
fajarandiy <fajarandi21@gmail.com>
2019-08-24 12:41:39 +0700

    First commit

---
+++ src/main/java/com/pikachugo/RestManager/RestManagerApplication.java
@@ -0,0 +1,14 @@
+package com.pikachugo.RestManager;
+
+import org.springframework.boot.SpringApplication;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestManagerApplication.class, args);
+	}
+
+}
